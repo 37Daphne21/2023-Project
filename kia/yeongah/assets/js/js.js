1e8b6251af2db8e08c55fc657f333ec2c01ca4bc
Daphne <37Daphne21@gmail.com>
2023-09-28 22:02:04 +0900

    [kia/yeongah] nav mo 진행중

--- kia/yeongah/assets/js/js.js
+++ kia/yeongah/assets/js/js.js
@@ -1,3 +1,5 @@
 /*
-  1. .has-sub 클래스가 붙어있는 .menu > li에 hover할 경우에만 .header에 .sub-open 클래스 추가
+  1. [JS] .has-sub 클래스가 붙어있는 .menu > li에 hover할 경우에만 .header에 .sub-open 클래스 추가
+  2. [JS] .toggle__link 클릭 시 .header__nav에 .is-open 클래스 추가
+  3. [JS] Only MO : li.has-sub 활성화 시 .is-open 클래스 추가
 */
