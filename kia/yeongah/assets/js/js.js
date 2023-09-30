cd1e055568cb290377310cec0792dac85d0a3a92
Daphne <37Daphne21@gmail.com>
2023-09-30 12:54:40 +0900

    [kia/yeongah] header / footer mo 완료

--- kia/yeongah/assets/js/js.js
+++ kia/yeongah/assets/js/js.js
@@ -1,5 +1,7 @@
 /*
   1. [JS] .has-sub 클래스가 붙어있는 .menu > li에 hover할 경우에만 .header에 .sub-open 클래스 추가
-  2. [JS] Only MO : .toggle__link 클릭 시 .header__nav에 .is-open 클래스 추가
+  2. [JS] Only MO : .toggle__link 클릭 시 .header__nav에 .is-open 클래스 추가 / body 태그에 .prevent-scroll 클래스 추가
   3. [JS] Only MO : li.has-sub 활성화 시 .is-open 클래스 추가
+
+  (가능 시 추가) header의 .util과 footer의 .util 마크업 동일. Mo 화면에서 footer의 .util을 header 안으로 삽입되도록 구현 필요
 */
